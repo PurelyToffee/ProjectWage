ef923d65d0b95a6c1e4ea534e1336928e281fed6
Toffee <147815799+PurelyToffee@users.noreply.github.com>
2026-03-15 23:07:19 +0000

    fake merge

---
+++ Scripts/rocket/RocketLauncherComponent.gd
@@ -0,0 +1,22 @@
+class_name RocketLauncherComponent extends Node
+
+@export var rocket_scene: PackedScene
+
+const MAX_ROCKETS := 4;
+var rockets = MAX_ROCKETS;
+
+var rockets_reload_speed = 0.5;
+
+
+func update(delta : float) -> void:
+	
+	rockets = clamp(rockets + rockets_reload_speed * delta, 0, MAX_ROCKETS)
+
+func launch_rocket() -> void:
+	
+	if floor(rockets) < 1: return;
+	rockets -= 1;
+	
+	var rocket = rocket_scene.instantiate()
+	rocket.global_transform = Global.player_attack_origin.global_transform
+	get_tree().current_scene.add_child(rocket)
---
+++ Scripts/weapons/WeaponStats.gd
@@ -0,0 +1,11 @@
+class_name WeaponStats
+extends Resource
+
+# base values
+@export var weapon_name := "Weapon"
+@export var fire_rate := 0.5
+@export var damage := 10.0
+@export var max_ammo := 30
+@export var spread := 0.0
+@export var fire_range := 50.0
+@export var reload_time := 1.5
