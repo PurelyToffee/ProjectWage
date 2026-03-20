295a1c971bf4a449bc905e23c0a4d62ce2416817
Pedro Gorobey <up202210292@up.pt>
2026-03-20 14:07:12 +0000

    Timer can now freeze/reset and added time taken on win screen

--- Scripts/LevelController.gd
+++ Scripts/LevelController.gd
@@ -5,29 +5,23 @@ var player_frozen : bool = false;
 var player : CharacterBody3D;
 var player_attack_origin : Node3D;
 var player_camera : Camera3D;
-var level_timer : float;
+var level_timer : float = 0.0;
+var timer_frozen : bool = false;
 
 var hud : CanvasLayer;
 
 const DualMacTen = preload("uid://bolqjo6l5kov7")
 
 func time_to_str(time: float = level_timer):
-	# var hours: String = str(floor(time / (60*60)))
-	var minutes: String = str(int(floor(time / 60.0)) % 60)
-	var seconds: String = str(int(floor(time)) % 60)
-	var millis: String = str(int(floor(time * 1000.0)) % 1000)
-	if minutes.length() < 2:
-		minutes = "0" + minutes
-	if seconds.length() < 2:
-		seconds = "0" + seconds
-	if millis.length() < 3:
-		millis = "0" + millis
-		if millis.length() < 3:
-			millis = "0" + millis
-	return minutes + ":" + seconds + ":" + millis
+	return "%02d:%02d:%03d" % [
+		int(floor(time / 60.0)) % 60,
+		int(floor(time)) % 60,
+		int(floor(time * 1000.0)) % 1000,
+	]
 
 func _process(delta : float) -> void:
-	level_timer += delta;
+	if !timer_frozen:
+		level_timer += delta;
 	if Input.is_action_just_pressed("launch_enemy"):
 		load_checkpoint()
 
@@ -46,7 +40,7 @@ func load_checkpoint(ent : CharacterBody3D = player) -> void:
 	
 	if !current_checkpoint:
 		return;
-		
+	
 	current_checkpoint.respawn_entity(ent);
 
 func reset_level() -> void:
@@ -54,6 +48,8 @@ func reset_level() -> void:
 	set_checkpoint(null)
 	
 	freeze_player(false)
+	timer_frozen = false
+	set_timer(0.0)
 	
 
 #endregion
@@ -69,8 +65,15 @@ func end_level() -> void:
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	freeze_player(true)
+	timer_frozen = true
 	
 	pass;
+
+func set_timer(time: float) -> void:
+	level_timer = time
+
+func freeze_timer(val: bool) -> void:
+	timer_frozen = val
 
 func player_is_crouched():
 	return player.is_crouched;
