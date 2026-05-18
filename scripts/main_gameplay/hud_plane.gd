a4786d173f3a198b73bf028b2d0e360f3603d780
GuilhermeGuerra2004 <147815799+GuilhermeGuerra2004@users.noreply.github.com>
2026-05-18 16:08:33 +0100

    Still trying to fix it lmao

--- scripts/main_gameplay/hud_plane.gd
+++ scripts/main_gameplay/hud_plane.gd
@@ -4,9 +4,4 @@ extends MeshInstance3D
 @export var viewport: SubViewport
 
 func _ready() -> void:
-	var mat := StandardMaterial3D.new()
-
-	mat.albedo_texture = viewport.get_texture()
-	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-
-	set_surface_override_material(0, mat)
+	pass
