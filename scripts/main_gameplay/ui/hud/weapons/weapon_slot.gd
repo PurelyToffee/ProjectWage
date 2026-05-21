34ad9ee361db81fbdf24c9120e5f5c4afcb98ca8
Toffee <147815799+PurelyToffee@users.noreply.github.com>
2026-05-21 14:26:58 +0100

    Added mac10 and grenade launcher sprites for hud.

--- scripts/main_gameplay/ui/hud/weapons/weapon_slot.gd
+++ scripts/main_gameplay/ui/hud/weapons/weapon_slot.gd
@@ -2,6 +2,10 @@ class_name WeaponSlot extends GameplayHudElement
 
 @onready var progress_bar_outline: NinePatchRect = %ProgressBarOutline
 @onready var sub_viewport_container: SubViewportContainer = $SubViewportContainer
+@onready var animated_sprite_2d: AnimatedSprite2D = $SubViewportContainer/SubViewport/AnimatedSprite2D
+
+func _ready() -> void:
+	animated_sprite_2d.play("default")
 
 func set_alpha(val : float) -> void:
 	sub_viewport_container.modulate.a = val;
