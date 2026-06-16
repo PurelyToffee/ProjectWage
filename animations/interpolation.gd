f760075a321877f5009933cbda2a81bd97842902
Toffee <147815799+PurelyToffee@users.noreply.github.com>
2026-06-16 16:41:14 +0100

    Started with wall run segments

---
+++ animations/interpolation.gd
@@ -0,0 +1,27 @@
+@tool     # makes script run from within the editor
+extends EditorScript    # gives you access to editor functions
+
+
+
+func _run():   # this is the main function
+	var selection = get_editor_interface().get_selection() # the selected node. In your case, select the AnimationPlayer
+	selection = selection.get_selected_nodes()  # get the actual AnimationPlayer node
+	if selection.size()!=1 and not selection is AnimationPlayer: # if the wrong node is selected, do nothing
+		return
+	else:
+		interpolation_change(selection[0]) # run the funstion in question
+
+
+
+func interpolation_change(selection : AnimationPlayer):
+	print(selection.get_animation_list())
+	
+	var tracks = selection.get_animation_list();
+	
+	for t in tracks:
+		var anim_track_1 = selection.get_animation(t) # get the Animation that you are interested in (change "default" to your Animation's name)
+		var count  = anim_track_1.get_track_count() # get number of tracks (bones in your case)
+		for i in count:
+			
+			anim_track_1.track_set_interpolation_type(i, 0) # change interpolation mode for every track
+			print(anim_track_1.track_get_interpolation_type(i))
