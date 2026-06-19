44f9088598a4b6421b736f0e552aa019c4f5f804
Toffee <147815799+PurelyToffee@users.noreply.github.com>
2026-06-19 18:36:05 +0100

    Fixed some small issues with loading screen.

--- scripts/menus/loading_screen/loading_screen.gd
+++ scripts/menus/loading_screen/loading_screen.gd
@@ -12,12 +12,20 @@ func start(on_visible: Callable) -> void:
 	tween.tween_method(func(a: float):
 		loading_group.material.set_shader_parameter("alpha", a)
 	, 0.0, 1.0, 1.)
-	tween.tween_callback(on_visible)
+	
+	tween.tween_callback(
+		func(): 
+			var min_time := get_tree().create_timer(0.5).timeout
+			await min_time
+			on_visible.call()
+	)
 
 func load(on_loaded: Callable) -> void:
+	
 	on_loaded.call()
 
 func finish() -> void:
+	
 	var tween = create_tween()
 	tween.tween_method(func(a: float):
 		loading_group.material.set_shader_parameter("alpha", a)
@@ -25,3 +33,4 @@ func finish() -> void:
 	tween.tween_callback(func():
 		visible = false
 	)
+	
