4c3071b47be9a6671bf801eafa01ee0c4002b889
Toffee <147815799+PurelyToffee@users.noreply.github.com>
2026-06-21 10:05:25 +0100

    Updated the particles for the cigarette.

--- scripts/menus/main_menu/wage_backdrop/menu_animation_wage.gd
+++ scripts/menus/main_menu/wage_backdrop/menu_animation_wage.gd
@@ -1,4 +1,12 @@
 extends Node3D
 
+@onready var world_environment = %WorldEnvironment
+
 func _process(delta: float) -> void:
-	visible = GameController.game_state == GameController.game_states.main_menu
+
+	if GameController.game_state == GameController.game_states.main_menu and world_environment.environment == null:
+		visible = true;
+		world_environment.environment = load("uid://dy6udaxbuduwv")
+	elif GameController.game_state != GameController.game_states.main_menu and world_environment.environment != null:
+		visible = false;
+		world_environment.environment = null;
